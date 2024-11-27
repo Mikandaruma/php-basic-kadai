9be266cd2c75cc2ba7274c31ba4c7b89c7f824a3
Mikandaruma <minori1277@gmail.com>
2024-11-27 14:33:41 +0900

    課題007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf8">
+    <title>課題007</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        //連想配列の値を代入する
+        $vegitable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160',];
+
+        //連想配列の値を出力する
+        print_r($vegitable_data);
+
+        ?>
+
+</p>
+
+</body>
+
